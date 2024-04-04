d2fdef9989561496722b42d41780aa414718873c
Yamini20000 <ext-ych@ecco.com>
2024-04-04 13:14:52 +0530

    implementation of live streaming using mediasoup

--- dist/user/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const signin_1 = require("./signin");
-const signout_1 = require("./signout");
-const stream_1 = require("./stream");
-const router = (0, express_1.Router)();
-router.use('/', signin_1.signinRouter, signout_1.signoutRouter, stream_1.userStreamRouter);
-exports.default = router;
